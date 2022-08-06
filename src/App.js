8b4f40c87fc3db201d116d461ac44e2ec4775d32
Vernon kacelo <vernonkachelo@gmail.com>
2022-08-06 18:53:57 +0200

    added pomo doro clock with start, pause and reset buttons

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import { CreateTodo } from "./components/createTodo";
 import "./components/styles.scss";
 import Home from "./components/home";
 import TodoHome from "./components/todo-home";
+import Pomo from "./components/pomo-doro/pomo-doro";
 function App() {
   return (
     <Router>
@@ -17,6 +18,7 @@ function App() {
           <Route  path="/showTodoList" element={<ShowTodoList />} />
           <Route path="/createTodo" element={<CreateTodo />} />
           <Route path="/todo-home"  element={<TodoHome/>}/>
+          <Route path="/pomo-doro/pomo-doro"  element={<Pomo/>}/>
         </Routes>
       </div>
       {/* <CreateTodo/> */}
