e04c98c54e4ea6586c3f2d06a1f168daf6e9044e
Vernon kacelo <vernonkachelo@gmail.com>
2022-08-26 19:45:29 +0200

    added weather component

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -4,13 +4,20 @@ import { getCurrentdate } from "../helpers/current-day";
 import { greetUser } from "../helpers/greeting";
 import { NavLink } from "react-router-dom";
 import Pomo from "./pomo-doro/pomo-doro";
+import Weather from "./weather-component/weather-component";
 const Home = () => {
   return (
     <Grid className="home">
+    
+    
+       
       <Grid.Column className="home-container">
+        
+        <Weather location={'Windhoek'} />
         <h3 className="current-date">{getCurrentdate()}</h3>
         <h2>{greetUser()}</h2>
         <h2>What would you like to do today?</h2>
+        
         <div className="options-container">
           <Button className="options-button">
             <NavLink className="p-2" to="/todo-home" exact>
@@ -27,8 +34,9 @@ const Home = () => {
           <Button className="options-button">Timer</Button>
         </div>
         <div className="motivate">motivational message</div>
-        <Pomo />
+        
       </Grid.Column>
+      
     </Grid>
   );
 };
